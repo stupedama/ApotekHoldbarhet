6564680603284dffd6b4586eb063628f94875ed7
Fredrik Fjeldvær <fredrik@ffj.no>
2019-03-24 00:19:49 +0100

    comment
    added a comment to the function

--- app/src/check_numbers.cpp
+++ app/src/check_numbers.cpp
@@ -4,6 +4,8 @@
 namespace apotek {
 namespace apotekholdbarhet{
 
+// checks if the QString is a number or not
+// returns true if string is only numbers, otherwise returns false.
 bool check_numbers(const QString& s)
 {
     const QRegExp check_string("\\d*");
