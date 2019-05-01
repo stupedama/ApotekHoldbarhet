8ba1fa4fba6e60c675ec8b289912ced812b23b82
Fredrik Fjeldvær <fredrik@ffj.no>
2019-05-01 19:19:54 +0200

    QStrings moved to inside while loop

--- app/src/fest_xml/fest_reader.h
+++ app/src/fest_xml/fest_reader.h
@@ -65,32 +65,29 @@ inline void FEST_Reader::read_file()
     XMLError read_file_error = doc.LoadFile(m_filename.c_str());
     if(read_file_error != XML_SUCCESS) return;
 
-
-    // -1 means no value
-    QString id{"-1"};
-    QString navn{"-1"};
-    int varenr{-1};
-    QString ean{"-1"};
-    QString legemiddelform{"-1"};
-    int mengde{-1};
-
     XMLNode* node = doc.FirstChildElement("FEST")->FirstChildElement("KatLegemiddelpakning")->FirstChildElement("OppfLegemiddelpakning");
     if(node == nullptr) return;
 
+    // QString -1 or int -1 means error.
     while(node != nullptr) {
+        QString id = node->FirstChildElement("Id")->GetText();
+        QString navn = node->FirstChildElement("Legemiddelpakning")->FirstChildElement("NavnFormStyrke")->GetText();
+        int varenr = node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Varenr")->IntText();
 
-        id = node->FirstChildElement("Id")->GetText();
-        navn = node->FirstChildElement("Legemiddelpakning")->FirstChildElement("NavnFormStyrke")->GetText();
-        varenr = node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Varenr")->IntText();
-
-        if(node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Ean"))
+        QString ean{"-1"};
+        if(node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Ean")) {
             ean = node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Ean")->GetText();
+        }
 
-        if(node->FirstChildElement("Legemiddelpakning")->FirstChildElement("LegemiddelformKort")->FindAttribute("DN"))
+        QString legemiddelform{"-1"};
+        if(node->FirstChildElement("Legemiddelpakning")->FirstChildElement("LegemiddelformKort")->FindAttribute("DN")) {
             legemiddelform = node->FirstChildElement("Legemiddelpakning")->FirstChildElement("LegemiddelformKort")->FindAttribute("DN")->Value();
+        }
 
-        if(node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Pakningsinfo")->FirstChildElement("Mengde"))
+        int mengde{-1};
+        if(node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Pakningsinfo")->FirstChildElement("Mengde")) {
             mengde = node->FirstChildElement("Legemiddelpakning")->FirstChildElement("Pakningsinfo")->FirstChildElement("Mengde")->IntText();
+        }
 
         Product v(id, navn, varenr, ean, legemiddelform, mengde);
         m_varer.push_back(v);
